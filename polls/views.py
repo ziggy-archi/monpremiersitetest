33e4f362ceaad413e7bb9ae202d9197c35389190
ziggy-archi <ziggyarchi@hotmail.fr>
2015-03-03 15:58:30 +0100

    ajout de l'index

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, Http404
+
+from django.template import RequestContext, loader
+
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
+
+
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
 # Create your views here.
