c6aeecd3c79ddbd8088c9ca2521e8d9a81fd3a25
ziggy-archi <ziggyarchi@hotmail.fr>
2015-03-09 15:51:59 +0100

    fin

--- monpremiersitetest/settings.py
+++ monpremiersitetest/settings.py
@@ -27,7 +27,7 @@ TEMPLATE_DEBUG = True
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,8 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+
     'polls',
+    'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,4 +85,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- polls/views.py
+++ polls/views.py
@@ -6,11 +6,34 @@ from django.template import RequestContext, loader
 
 from django.shortcuts import render, get_object_or_404
 
-from polls.models import Question
-
 from django.core.urlresolvers import reverse
 
 from polls.models import Choice, Question
+
+from django.views import generic
+
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+         return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def detail(request, question_id):
